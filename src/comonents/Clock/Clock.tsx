e24fde17dbabed0d165a8b0f8eff7b94c2bcec37
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-01 23:15:03 +0000

    using svg to draw a circle and update its value based on the time state

--- src/comonents/Clock/Clock.tsx
+++ src/comonents/Clock/Clock.tsx
@@ -1,24 +1,40 @@
 import React from "react";
+import useCountdown from "../../hooks/useCountdown";
+import formatMillisecondsToTime from "../../utils/formatMillisecondsToTime";
 
 export default function Clock() {
+  const [time, pause, resume] = useCountdown(25000000000);
+  const timeString = formatMillisecondsToTime(time);
+
   return (
-    <div className="flex items-center justify-center mt-16">
+    <div className="flex items-center justify-center mt-8">
       <svg
-        className="-rotate-90"
+        className=""
         strokeDasharray={1005}
-        strokeDashoffset={900}
-        height="400"
-        width="400"
+        strokeDashoffset={100}
+        height="360"
+        width="360"
       >
         <circle
-          cx="200"
-          cy="200"
+          cx="180"
+          cy="180"
           r="160"
           stroke="#e2e8f0"
           strokeWidth="10"
           strokeLinecap="round"
           fill="#334155"
         />
+        <text
+          x="180"
+          y="180"
+          fill="#e2e8f0"
+          textAnchor="middle"
+          fontSize={"2rem"}
+          alignmentBaseline="middle"
+          fontFamily="Helvetica"
+        >
+          {timeString}
+        </text>
       </svg>
     </div>
   );
