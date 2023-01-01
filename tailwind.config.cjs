d6b2d0e215b69c10b4ce2d0238f7e82b4b5601d8
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-01 23:16:36 +0000

    removed daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        offsettozero: {},
+      },
+      animation: {},
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
