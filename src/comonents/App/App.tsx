9df69ff3e0029d0aae18ffcab8aae3fbedc3880c
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-01 23:16:18 +0000

    added burger navigation icon

--- src/comonents/App/App.tsx
+++ src/comonents/App/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "../Button/Button";
 import Clock from "../Clock/Clock";
 import TimerModeDisplay from "../TimerModeDisplay/TimerModeDisplay";
-import reactLogo from "./assets/react.svg";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +11,11 @@ function App() {
     <div className="flex flex-col min-h-screen p-3 items-center bg-slate-900">
       <div className="flex flex-col max-w-3xl flex-1 w-full text-center">
         <h1 className="text-5xl text-slate-200 font-bold">Pomodoro Timer</h1>
+        <div className="flex justify-end">
+          <RxHamburgerMenu size={40} className="text-slate-200" />
+        </div>
         <TimerModeDisplay />
         <Clock />
-        <Button>Settings</Button>
       </div>
     </div>
   );
